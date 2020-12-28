c8d84c87268df2eba64b50238025cf38ddf60c38
Doris-0327 <75714539+Doris-0327@users.noreply.github.com>
2020-12-28 20:12:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -118,15 +118,15 @@ class Farm(tk.Frame):  # try
 
         # grid 上去
         #sticky = tk.N+tk.S
-        self.button_seed_package_pepper.grid(row = 1, column = 0)
-        self.button_seed_package_eggplant.grid(row = 1, column = 1)
-        self.button_seed_package_coriander.grid(row = 1, column = 2)
+        self.button_seed_package_pepper.grid(row = 1, column = 1)
+        self.button_seed_package_eggplant.grid(row = 1, column = 2)
+        self.button_seed_package_coriander.grid(row = 1, column = 3)
         lab1 = tk.Label(r1, text='青椒種子', height=1, width=15, bg='white', fg='black')
-        lab1.grid(row=2, column=0)
+        lab1.grid(row=2, column=1)
         lab2 = tk.Label(r1, text='茄子種子', height=1, width=15, bg='white', fg='black')
-        lab2.grid(row=2, column=1)
+        lab2.grid(row=2, column=2)
         lab3 = tk.Label(r1, text='香菜種子', height=1, width=15, bg='white', fg='black')
-        lab3.grid(row=2, column=2)
+        lab3.grid(row=2, column=3)
 
 
     # 種子商店功能-青椒種子
